8d21b2a87e60426f1c0487da7f98b4c4a8602c90
Kobi Eshun <kobi.eshun@spiff.com>
2023-04-21 00:32:19 -0400

    Allow elixir version 1.11

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Handler.MixProject do
     [
       app: :handler,
       version: @version,
-      elixir: "~> 1.12",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: [
